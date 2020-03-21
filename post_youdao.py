9057119e9f5442d9359697c3baa012360fdf6754
unknown <imiaom@hotmail.com>
2020-03-21 16:57:20 +0800

    add youdao

---
+++ post_youdao.py
@@ -0,0 +1,21 @@
+import requests
+
+
+url='http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+form_data={
+'i': '我和你',
+'from': 'AUTO',
+'to':'AUTO',
+'smartresult':'dict',
+'client':'fanyideskweb',
+'salt':'15847786751177',
+'sign':'efcee979dd768aebfd1cb13e9befa70b',
+'ts':'1584778675117',
+'bv':'70244e0061db49a9ee62d341c5fed82a',
+'doctype':'json',
+'version':'2.1',
+'keyfrom':'fanyi.web',
+'action':'FY_BY_CLICKBUTTION'
+}
+reponse=requests.post(url,form_data)
+print(reponse.text)
